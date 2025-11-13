14faf4322918b707b073e3facc8dae14313850d9
Elie Daher <143806470+ElieDaher07@users.noreply.github.com>
2025-11-13 10:12:27 -0500

    Fini

---
+++ 582-531-cours11-osc-unity-flappybird/Assets/flappybird/Scripts/Player.cs
@@ -0,0 +1,117 @@
+﻿using UnityEngine;
+using extOSC;
+
+
+public class Player : MonoBehaviour
+{
+    public Sprite[] sprites;
+    public float strength = 5f;
+    public float gravity = -9.81f;
+    public float tilt = 5f;
+    public int value;
+
+    private SpriteRenderer spriteRenderer;
+    private Vector3 direction;
+    private int spriteIndex;
+    private int etatEnMemoire = 1;
+
+    public extOSC.OSCReceiver oscReceiver;
+
+    void TraiterOscKey(OSCMessage message)
+    {
+        // Si le message n'a pas d'argument ou l'argument n'est pas un Int on l'ignore
+        if (message.Values.Count == 0)
+        {
+            Debug.Log("No value in OSC message");
+            return;
+        }
+
+        if (message.Values[0].Type != OSCValueType.Int)
+        {
+            Debug.Log("Value in message is not an Int");
+            return;
+        }
+
+        // Récupérer la valeur de l’angle depuis le message OSC
+        value = message.Values[0].IntValue;
+
+        int nouveauEtat = value; // REMPLACER ici les ... par le code qui permet de récupérer la nouvelle donnée du flux
+        if (etatEnMemoire != nouveauEtat)
+        { // Le code compare le nouvel etat avec l'etat en mémoire
+            etatEnMemoire = nouveauEtat; // Le code met à jour l'état mémorisé
+            if (nouveauEtat == 0)
+            {
+                direction = Vector3.up * strength;
+                
+                // METTRE ici le code pour lorsque le bouton est appuyé
+            }
+            else
+            {
+                
+                // METTRE ici le code pour lorsque le bouton est relaché
+            }
+        }
+    }
+
+    private void Awake() { 
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Start() { 
+        InvokeRepeating(nameof(AnimateSprite), 0.15f, 0.15f);
+
+        oscReceiver.Bind("/key", TraiterOscKey);
+    }
+
+    private void OnEnable()
+    {
+        Vector3 position = transform.position;
+        position.y = 0f;
+        transform.position = position;
+        direction = Vector3.zero;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        {
+            direction = Vector3.up * strength;
+        }
+
+        // Apply gravity and update the position
+        direction.y += gravity * Time.deltaTime;
+        transform.position += direction * Time.deltaTime;
+
+        // Tilt the bird based on the direction
+        Vector3 rotation = transform.eulerAngles;
+        rotation.z = direction.y * tilt;
+        transform.eulerAngles = rotation;
+    }
+
+    private void AnimateSprite()
+    {
+        spriteIndex++;
+
+        if (spriteIndex >= sprites.Length)
+        {
+            spriteIndex = 0;
+        }
+
+        if (spriteIndex < sprites.Length && spriteIndex >= 0)
+        {
+            spriteRenderer.sprite = sprites[spriteIndex];
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Obstacle"))
+        {
+            GameManager.Instance.GameOver();
+        }
+        else if (other.gameObject.CompareTag("Scoring"))
+        {
+            GameManager.Instance.IncreaseScore();
+        }
+    }
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,72 @@
 #include <Arduino.h>
+#include <FastLED.h>
+#include <MicroOscSlip.h>
+#include <M5_PbHub.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define BROCHE_BOUTON_ATOM_LUMIERE 27
+
+#define BROCHE_BOUTON_KEY 32
+#define BROCHE_BOUTON_KEY_LUMIERE 26
+#define KEY_CHANNEL 0
+
+CRGB pixelAtom;
+MicroOscSlip<128> monOsc(&Serial);
+M5_PbHub myPbHub;
+
+unsigned long monChronoDepart;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  Wire.begin();
+  myPbHub.begin();
+
+  FastLED.addLeds< WS2812,  BROCHE_BOUTON_ATOM_LUMIERE , GRB >(&pixelAtom, 1); 
+
+  myPbHub.setPixelCount( KEY_CHANNEL , 1);
+
+  pixelAtom = CRGB(255,0,0);
+  FastLED.show();
+  delay(1000);
+  pixelAtom = CRGB(255,255,0);
+  FastLED.show();
+  delay(1000);
+  pixelAtom = CRGB(0,255,0);
+  FastLED.show();
+  delay(1000);
+  pixelAtom = CRGB(0,0,0);
+  FastLED.show();
+
 }
+
+// FONCTION QUI SERA APPELÉE LORSQU'UN N'IMPORTTE QUEL MESSAGE OSC EST REÇU
+// receivedOscMessage est le message reçu
+void myOscMessageParser(MicroOscMessage & receivedOscMessage) {
+  // Ici, un if et receivedOscMessage.checkOscAddress() est utilisé pour traiter les différents messages
+  if (receivedOscMessage.checkOscAddress("/pixel")) {  // MODIFIER /pixel pour l'adresse qui sera reçue
+       int premierArgument = receivedOscMessage.nextAsInt(); // Récupérer le premier argument du message en tant que int
+       int deuxiemerArgument = receivedOscMessage.nextAsInt(); // SI NÉCESSAIRE, récupérer un autre int
+       int troisiemerArgument = receivedOscMessage.nextAsInt(); // SI NÉCESSAIRE, récupérer un autre int
+
+       // UTILISER ici les arguments récupérés
+       myPbHub.setPixelColor( KEY_CHANNEL , 0 , premierArgument , deuxiemerArgument , troisiemerArgument );
+        
+   }
+}
+
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+  int key = myPbHub.digitalRead(KEY_CHANNEL);
+
+  monOsc.onOscMessageReceived(myOscMessageParser);
+
+  if ( millis() - monChronoDepart >= 20 ) { 
+    monChronoDepart = millis();
+
+    monOsc.sendInt("/key", key);
+
+
+ 
+  }
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
